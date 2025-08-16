4811fb8022f2ef283ea4aee18953bfd47b5c27d4
Memiş Ali Tufan <67204706+Tufan17@users.noreply.github.com>
2025-08-16 20:49:28 +0300

    added

---
+++ src/db/migrations/20250816141416_solution_partner.ts
@@ -0,0 +1,31 @@
+import type { Knex } from "knex";
+
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("solution_partners", (table) => {
+        table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
+        table.uuid("location_id").notNullable().references("id").inTable("cities");
+        table.string("name").notNullable();
+        table.string("phone").notNullable();
+        table.string("address").nullable();
+        table.string("tax_office").nullable();
+        table.string("tax_number").nullable();
+        table.string("bank_name").nullable();
+        table.string("swift_number").nullable();
+        table.string("account_owner").nullable();
+        table.string("iban").nullable();
+        table.string("language_code").notNullable();
+        table.boolean("admin_verified").notNullable().defaultTo(false);
+        table.boolean("status").notNullable().defaultTo(false);
+        table.string("created_at").defaultTo(knex.fn.now());
+        table.string("updated_at").defaultTo(knex.fn.now());
+        table.string("deleted_at").nullable();
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable("solution_partners");
+}
+
