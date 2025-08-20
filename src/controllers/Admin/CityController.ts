db1816cd49781614a4fb8170f874a1a9c2df5318
memisalitufan <alitufan.asidev@gmail.com>
2025-08-20 17:03:18 +0300

    added

--- src/controllers/Admin/CityController.ts
+++ src/controllers/Admin/CityController.ts
@@ -35,7 +35,21 @@ export default class CityController {
         .select("cities.*", "city_pivots.name as name")
         .groupBy("cities.id", "city_pivots.name");
 
-      const countResult = await query.clone().count("* as total").first();
+      const countQuery = knex("cities")
+        .whereNull("cities.deleted_at")
+        .innerJoin("city_pivots", "cities.id", "city_pivots.city_id")
+        .where("city_pivots.language_code", language)
+        .where(function () {
+          if (country_id) {
+            this.where("cities.country_id", country_id);
+          }
+        })
+        .where(function () {
+          this.where("city_pivots.name", "ilike", `%${search}%`);
+        })
+        .countDistinct("cities.id as total");
+
+      const countResult = await countQuery.first();
       const total = Number(countResult?.total ?? 0);
       const totalPages = Math.ceil(total / Number(limit));
       const data = await query
