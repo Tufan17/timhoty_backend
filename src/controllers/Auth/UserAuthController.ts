a871381a3aeb6da2145ff91925b595c2057e4db5
Memiş Ali Tufan <67204706+Tufan17@users.noreply.github.com>
2025-10-01 11:24:34 +0300

    added

--- src/controllers/Auth/UserAuthController.ts
+++ src/controllers/Auth/UserAuthController.ts
@@ -46,7 +46,7 @@ export default class UserAuthController {
 	// User register
 	async register(req: FastifyRequest, res: FastifyReply) {
 		try {
-			const { name_surname, email, password, language } = req.body as { name_surname: string; email: string; password: string; language: string }
+			const { name_surname, email, password, language="en" } = req.body as { name_surname: string; email: string; password: string; language: string }
 			const user = await new AuthUserService().register(name_surname, email, password, language, req.t)
 			if (!user.success) {
 				return res.status(400).send({
