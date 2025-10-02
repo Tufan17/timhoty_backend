cea09406782d999abdf0246eee04220dbfffe102
Memiş Ali Tufan <67204706+Tufan17@users.noreply.github.com>
2025-10-02 16:24:58 +0300

    added

--- src/db/migrations/20250817173809_campaign.ts
+++ src/db/migrations/20250817173809_campaign.ts
@@ -7,7 +7,7 @@ export async function up(knex: Knex): Promise<void> {
         table.string("start_date").notNullable();
         table.string("end_date").notNullable();
         table.string("photo_url").notNullable();
-        table.enum("service_type", ["hotel", "rental", "activity", "tour"]).notNullable().defaultTo("hotel");
+        table.enum("service_type", ["hotel", "rental", "activity", "tour", "visa"]).notNullable().defaultTo("hotel");
         table.boolean("highlight").notNullable().defaultTo(false);
         table.boolean("status").notNullable().defaultTo(true);
         table.timestamp("created_at").defaultTo(knex.fn.now());
