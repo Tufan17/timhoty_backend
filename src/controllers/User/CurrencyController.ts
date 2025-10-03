21b685a44c6e5c7cb1546b6154784c8e65c728b9
Yusuf Kaan <yukayu697@gmail.com>
2025-10-03 14:41:17 +0300

    kullanıcı duzeltilmesi yapıldı

--- src/controllers/User/CurrencyController.ts
+++ src/controllers/User/CurrencyController.ts
@@ -1,21 +1,21 @@
-import { FastifyReply, FastifyRequest } from "fastify";
-import knex from "../../db/knex";
+import { FastifyReply, FastifyRequest } from "fastify"
+import knex from "../../db/knex"
 
 export default class UserCurrencyController {
-    async getAll(req: FastifyRequest, res: FastifyReply) {
-        try {
-            const currencies = await knex("currencies").where("is_active", true).whereNull("deleted_at");
-            return res.status(200).send({
-                success: true,
-                message: "Currencies fetched successfully",
-                data: currencies,
-            });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).send({
-                success: false,
-                message: "Error fetching currencies",
-            });
-        }
-    }
+	async getAll(req: FastifyRequest, res: FastifyReply) {
+		try {
+			const currencies = await knex("currencies").where("currencies.is_active", true).whereNull("currencies.deleted_at").innerJoin("currency_pivots", "currencies.id", "currency_pivots.currency_id").where("currency_pivots.language_code", req.language).whereNull("currency_pivots.deleted_at")
+			return res.status(200).send({
+				success: true,
+				message: "Currencies fetched successfully",
+				data: currencies,
+			})
+		} catch (error) {
+			console.log(error)
+			return res.status(500).send({
+				success: false,
+				message: "Error fetching currencies",
+			})
+		}
+	}
 }
--- src/validators/faq.ts
+++ src/validators/faq.ts
@@ -1,17 +1,15 @@
-import Joi from 'joi';
+import Joi from "joi"
 
 export const faqSchema = Joi.object({
-  title: Joi.string().required(),
-  description: Joi.string().required(),
-  order: Joi.string().required(),
-  service_type: Joi.string().valid("hotel", "rental", "activity", "tour").optional().default("hotel"),
-});
+	title: Joi.string().required(),
+	description: Joi.string().required(),
+	order: Joi.string().required(),
+	service_type: Joi.string().valid("hotel", "rental", "activity", "tour", "visa").optional().default("hotel"),
+})
 
 export const faqUpdateSchema = Joi.object({
-  title: Joi.string().optional(),
-  description: Joi.string().optional(),
-  order: Joi.string().optional(),
-  service_type: Joi.string().valid("hotel", "rental", "activity", "tour").optional(),
-});
-
-
+	title: Joi.string().optional(),
+	description: Joi.string().optional(),
+	order: Joi.string().optional(),
+	service_type: Joi.string().valid("hotel", "rental", "activity", "tour", "visa").optional(),
+})
