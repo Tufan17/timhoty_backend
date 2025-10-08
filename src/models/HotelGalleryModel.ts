6c0a4cd7b04e77910a9553225f9dd7128274fa63
Memiş Ali Tufan <67204706+Tufan17@users.noreply.github.com>
2025-10-08 17:12:24 +0300

    update hotel

--- src/models/HotelGalleryModel.ts
+++ src/models/HotelGalleryModel.ts
@@ -1,4 +1,5 @@
 import BaseModel from "@/models/BaseModel";
+import knex from "@/db/knex";
 
 class HotelGalleryModel extends BaseModel {
   constructor() {
@@ -13,6 +14,22 @@ class HotelGalleryModel extends BaseModel {
     'updated_at',
     'deleted_at',
   ];
+
+  async existsCategory(hotel_id: string, category: string) {
+    return await knex("hotel_gallery_pivot").where({ hotel_id, category }).whereNull("deleted_at").first();
+  }
+
+  async hasCoverImage(hotel_id: string) {
+    return await knex("hotel_gallery_pivot")
+      .join("hotel_galleries", "hotel_gallery_pivot.hotel_gallery_id", "hotel_galleries.id")
+      .where({
+        "hotel_galleries.hotel_id": hotel_id,
+        "hotel_gallery_pivot.category": "Kapak Resmi",
+      })
+      .whereNull("hotel_galleries.deleted_at")
+      .whereNull("hotel_gallery_pivot.deleted_at")
+      .first();
+  }
 }
 
 export default HotelGalleryModel;
