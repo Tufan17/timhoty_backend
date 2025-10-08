9dac1e8421b9d07b56e579d0e2cf5dcea3d6bce3
Yusuf Kaan <yukayu697@gmail.com>
2025-10-08 22:13:58 +0300

    update tour

--- src/db/migrations/20250930034823_tour_reservation_users.ts
+++ src/db/migrations/20250930034823_tour_reservation_users.ts
@@ -1,29 +1,22 @@
-import type { Knex } from "knex";
-
-
+import type { Knex } from "knex"
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable("tour_reservation_users", (table) => {
-    table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"));
-    table
-      .uuid("tour_reservation_id")
-      .notNullable()
-      .references("id")
-      .inTable("tour_reservations")
-      .onDelete("CASCADE");
-    table.string("name").nullable();
-    table.string("surname").nullable();
-    table.string("birthday").nullable();
-    table.string("email").nullable();
-    table.string("phone").nullable();
-    table.enu("type", ["adult", "child"]).nullable();
-    table.string("age").nullable();
-    table.timestamp("created_at").defaultTo(knex.fn.now());
-    table.timestamp("updated_at").defaultTo(knex.fn.now());
-    table.timestamp("deleted_at").nullable();
-  });
+	return knex.schema.createTable("tour_reservation_users", table => {
+		table.uuid("id").primary().defaultTo(knex.raw("gen_random_uuid()"))
+		table.uuid("tour_reservation_id").notNullable().references("id").inTable("tour_reservations").onDelete("CASCADE")
+		table.string("name").nullable()
+		table.string("surname").nullable()
+		table.string("birthday").nullable()
+		table.string("email").nullable()
+		table.string("phone").nullable()
+		table.enu("type", ["adult", "child", "baby"]).nullable()
+		table.string("age").nullable()
+		table.timestamp("created_at").defaultTo(knex.fn.now())
+		table.timestamp("updated_at").defaultTo(knex.fn.now())
+		table.timestamp("deleted_at").nullable()
+	})
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable("tour_reservation_users");
+	return knex.schema.dropTable("tour_reservation_users")
 }
